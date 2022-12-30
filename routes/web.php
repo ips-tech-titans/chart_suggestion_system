e828c4ec2cec07cca99a2061e6af839b9fb999f8
ips-tech-titans <ipstechtitans@gmail.com>
2022-12-30 10:26:41 +0530

    add new controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/maincall', 'App\Http\Controllers\MainController@main')->name('maincall');
